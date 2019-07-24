dcb17b99ff8810b849b5b1baa8833174e7c9b88c
Szabo Vilmos <vili8899s@gmail.com>
2019-07-24 15:52:57 +0200

    v1.0.0

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { MdSend } from 'react-icons/md';
+const ExpenseForm = ({
+  charge,
+  amount,
+  handleCharge,
+  handleAmount,
+  handleSubmit,
+  edit
+}) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-center">
+        <div className="form-group">
+          <label htmlFor="expense">költség</label>
+          <input
+            type="text"
+            className="form-control"
+            id="charge"
+            name="charge"
+            placeholder="pl. bérlés"
+            value={charge}
+            onChange={handleCharge}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="amount">összeg</label>
+          <input
+            type="number"
+            className="form-control"
+            id="amount"
+            name="amount"
+            placeholder="pl. 10000"
+            value={amount}
+            onChange={handleAmount}
+          />
+        </div>
+      </div>
+      <button type="submit" className="btn">
+        {edit ? 'szerkesztés' : 'küldés'}
+        {/* submit  */}
+        <MdSend className="btn-icon" />
+      </button>
+    </form>
+  );
+};
+
+export default ExpenseForm;
